4fc98ad06e9c3274e174f3dd2a581eff61986b0c
NadyaCodes <nadyacorscadden@gmail.com>
2022-07-08 01:05:46 -0400

    post data to database

--- routes/routes.js
+++ routes/routes.js
@@ -6,8 +6,19 @@ const Model = require('../model/model')
 
 
 //Post Method
-router.post('/post', (req, res) => {
-  res.send('Post API')
+router.post('/post', async (req, res) => {
+  const data = new Model({
+      name: req.body.name,
+      age: req.body.age
+  })
+
+  try {
+      const dataToSave = await data.save();
+      res.status(200).json(dataToSave)
+  }
+  catch (error) {
+      res.status(400).json({message: error.message})
+  }
 })
 
 
@@ -19,7 +30,7 @@ router.get('/getAll', (req, res) => {
 
 //Get by ID Method
 router.get('/getOne/:id', (req, res) => {
-  res.send('Get by ID API')
+  res.send(req.params.id)
 })
 
 
